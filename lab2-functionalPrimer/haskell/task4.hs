2c65cf8f9ba327d0ca539f45303f638a095c1da9
Iman-elber <eiman.06@outlook.com>
2026-02-11 19:38:13 +0000

    append ! to prompt each time empty string entered

--- lab2-functionalPrimer/haskell/task4.hs
+++ lab2-functionalPrimer/haskell/task4.hs
@@ -4,12 +4,11 @@ ask prompt =
   putStrLn prompt
   line <- getLine
   if line == ""
-    then ask prompt
-    else if (line == "quit") then putStrLn("quitting..") --condition 
-  added for quitting
-  else do
-    putStrLn ("you said: " ++ reverse line)
-    ask prompt --needed for repeated inputs
+    then ask (prompt ++ "!") 
+    else if (line == "quit") then putStrLn("quitting..") --condition added for quitting
+    else do
+     putStrLn ("you said: " ++ reverse line)
+     ask prompt --needed for repeated inputs
 
 main :: IO ()
 main =
